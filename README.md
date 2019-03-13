a7daab49eedde04c1cd8cf32a6d2bad05bd94749
yangyao <yangyaohappy@outlook.com>
2019-03-13 17:56:09 +0800

    init commit
    first edition

---
+++ README.md
@@ -0,0 +1,19 @@
+# how to use
+
+## fork project
+
+    `github.com/yangyao/ssr_subscribe_tool`
+
+## login to www.netlify.com
+
+    login with your github account
+    add site from git
+
+## build command
+
+    `php -S 127.0.0.1`
+
+## add node
+
+    visit `{your netlify link }?node={your ssr link}`
+
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$app = new Slim\App();
+
+$app->get('/', function (\Slim\Http\Request $request, \Slim\Http\Response $response, $args) {
+	$plain = file_get_contents('node.txt');
+	$ssr = collect(explode(PHP_EOL,$plain))->filter(function($line){
+	    return \Illuminate\Support\Str::contains($line,"ssr://");
+    });
+    return $response->getBody()->write(base64_encode($ssr));
+});
+
+$app->get('/add', function (\Slim\Http\Request $request, \Slim\Http\Response $response, $args) {
+    $ssr = $request->getParam('node');
+    file_put_contents('node.txt',$ssr,FILE_APPEND);
+    return $response->getBody()->write('done !');
+});
+
+$app->run();
