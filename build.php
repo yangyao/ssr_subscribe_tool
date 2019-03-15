711543630582d454c2102cb71fdc522cd07896b1
yangyao <yangyaohappy@outlook.com>
2019-03-15 11:25:56 +0800

    html render bug fixed

--- build.php
+++ build.php
@@ -27,4 +27,4 @@ $ssr = collect(explode(PHP_EOL,$plain))->filter(function($line){
     return \Illuminate\Support\Str::contains($line,"ssr://");
 });
 
-return file_put_contents('dist/index.html',base64_encode($ssr));
+return file_put_contents('dist/index.html',base64_encode(implode(PHP_EOL,$ssr->all())));
